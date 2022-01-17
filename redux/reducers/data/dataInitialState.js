5bd9891fc71246b1a30c819340a446396cc057d3
Roman Kolmakov <cold.snowww@yahoo.com>
2022-01-17 17:19:53 +0700

    Change type of filter field to array

--- redux/reducers/data/dataInitialState.js
+++ redux/reducers/data/dataInitialState.js
@@ -4,9 +4,8 @@ export const dataInitialState = {
    status: loadingStatus.LOADING,
    data: null,
    dataType: null,
+   // Initial quote id is always 1
    quoteID: 1,
-   filter: {
-      key: null,
-      value: null,
-   },
+   // First key, second value, use with shallowEqual
+   filter: [],
 };
