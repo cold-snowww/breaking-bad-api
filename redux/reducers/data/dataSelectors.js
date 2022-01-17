0d98847071e6c8769a2d3274343dc4f9302c796e
Roman Kolmakov <cold.snowww@yahoo.com>
2022-01-17 17:48:48 +0700

    Add dataSelectors

---
+++ redux/reducers/data/dataSelectors.js
@@ -0,0 +1,35 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+// Easy selectors
+export const selectStatus = (state) => state.data.status;
+export const selectData = (state) => state.data.data;
+export const selectDataType = (state) => state.data.dataType;
+export const selectRandomQuoteID = (state) => state.data.quoteID;
+export const selectFilter = (state) => state.data.filter;
+
+// Memoized selectors
+// Select entities matching filters
+export const selectFilteredData = createSelector(
+   selectData,
+   selectFilter,
+   (data, filter) => {
+      const [key, value] = filter;
+      if (!data || !key || !value) return null;
+      return data.filter((entity) => String(entity[key]) === value);
+   }
+);
+
+// Select unique values of field
+export const selectUniqValues = createSelector(
+   selectData,
+   selectFilter,
+   (data, filter) => {
+      const key = filter[0];
+      if (!data || !key) return null;
+      return data.reduce((prev, entity) => {
+         const value = entity[key];
+         if (!prev.includes(value)) prev.push(value);
+         return prev;
+      }, []);
+   }
+);
