bc292c59400e8a4ddb25d62c41726cc75881e7d3
Roman Kolmakov <cold.snowww@yahoo.com>
2022-01-27 14:06:56 +0700

    Add Loader showing and page title functionality

--- src/components/WelcomePageLayout/WelcomePageLayout.js
+++ src/components/WelcomePageLayout/WelcomePageLayout.js
@@ -6,15 +6,34 @@ import { selectCashedImages } from '../../redux/reducers/app/appSelectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { shallowEqual } from 'react-redux';
 import { setImageCashed } from '../../redux/reducers/app/appSlice';
+import { selectStatus } from '../../redux/reducers/data/dataSelectors';
+import { loadingStatus } from '../../redux/common';
+import { useEffect } from 'react';
+import { setStatus } from '../../redux/reducers/data/dataSlice';
+import usePageTitle from '../../hooks/usePageTitle';
 
 export default function WelcomePageLayout() {
    const [scrolled, onScroll] = useElementScroll();
    const cashedImages = useSelector(selectCashedImages, shallowEqual);
+   const appStatus = useSelector(selectStatus);
    const dispatch = useDispatch();
+
+   usePageTitle('Breaking Bad - Welcome!');
+
+   // Seting loading status
+   useEffect(() => {
+      const imageCashed = cashedImages.includes(bgImage);
+      if (imageCashed && appStatus === loadingStatus.LOADING) {
+         dispatch(setStatus(loadingStatus.IDLE));
+      }
+      if (!imageCashed && appStatus !== loadingStatus.LOADING) {
+         dispatch(setStatus(loadingStatus.LOADING));
+      }
+   }, [cashedImages, appStatus, dispatch]);
 
    return (
       <>
-         <Loader shown={!cashedImages.includes(bgImage)} />
+         <Loader shown={appStatus === loadingStatus.LOADING} />
          <div className="WelcomePageLayout">
             <div
                className="WelcomePageLayout__wrapper"
