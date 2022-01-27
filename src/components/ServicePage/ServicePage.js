5ce344d27d8558f4f48e8f5841d272259b9c5707
Roman Kolmakov <cold.snowww@yahoo.com>
2022-01-27 14:01:53 +0700

    Add ServicePage common component

---
+++ src/components/ServicePage/ServicePage.js
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './ServicePage.scss';
+
+export default function ServicePage({ text, icon, title, homelink }) {
+   return (
+      <div className="ServicePage">
+         <div className="ServicePage__window">
+            <header className="ServicePage__header">
+               <h3 className="ServicePage__title">{title}</h3>
+               <div className="ServicePage__icon">{icon}</div>
+            </header>
+            <p className="ServicePage__text">
+               {text}
+               <br />
+               {homelink ? (
+                  <span>
+                     You can try to go to the{' '}
+                     <Link to="/" className="ServicePage__homelink">
+                        Homepage
+                     </Link>
+                     .
+                  </span>
+               ) : null}
+            </p>
+         </div>
+      </div>
+   );
+}
+
+ServicePage.propTypes = {
+   text: PropTypes.node.isRequired,
+   icon: PropTypes.element.isRequired,
+   title: PropTypes.string.isRequired,
+   homelink: PropTypes.bool,
+};
