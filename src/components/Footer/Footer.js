065be0a7f4cca300acc37b024705517aba6793d2
Roman Kolmakov <cold.snowww@yahoo.com>
2022-01-29 15:11:30 +0700

    Add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.scss';
+
+export default function Footer() {
+   return (
+      <footer className="Footer">
+         <address className="Footer__creator">
+            Designed by <a href="mailto:smtexx@hotmail.com">smTeXX</a>
+         </address>
+         <p className="Footer__copyright">&copy;2022-2030</p>
+      </footer>
+   );
+}
