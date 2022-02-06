09ac8c848cd55cbc24e36e9bdabb734787378b22
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-06 13:55:42 +0700

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import DataPage from './components/DataPage/DataPage';
 import NotFoundPage from './components/NotFoundPage/NotFoundPage';
 import WelcomePage from './components/WelcomePage/WelcomePage';
+import { dataTypes } from './redux/common';
 
 function App() {
    return (
       <HashRouter>
          <Routes>
             <Route path="/" element={<WelcomePage />} />
+            <Route
+               path="/character"
+               element={<DataPage dataType={dataTypes.CHARACTER} />}
+            />
+            <Route
+               path="/episode"
+               element={<DataPage dataType={dataTypes.EPISODE} />}
+            />
+            <Route
+               path="/quote"
+               element={<DataPage dataType={dataTypes.QUOTE} />}
+            />
             <Route path="*" element={<NotFoundPage />} />
          </Routes>
       </HashRouter>
