a2026825ccb0b4fc9b6b8b2de2ad73e7fe8a6c07
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-08 14:34:51 +0700

    Add Menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,10 @@
+import SearchBlock from '../SearchBlock/SearchBlock';
+import './Menu.scss';
+
+export default function Menu() {
+   return (
+      <div className="Menu">
+         <SearchBlock />
+      </div>
+   );
+}
