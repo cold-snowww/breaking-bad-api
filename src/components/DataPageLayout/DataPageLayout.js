5d6628cf12a5b1022571c4661b97ebd616b0c585
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-08 10:37:34 +0700

    Add menu functionality

--- src/components/DataPageLayout/DataPageLayout.js
+++ src/components/DataPageLayout/DataPageLayout.js
@@ -1,6 +1,12 @@
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 import { loadingStatus } from '../../redux/common';
+import { selectMenuStatus } from '../../redux/reducers/app/appSelectors';
+import { toggleMenuStatus } from '../../redux/reducers/app/appSlice';
 import { selectStatus } from '../../redux/reducers/data/dataSelectors';
+import Burger from '../Burger/Burger';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -9,6 +15,10 @@ import './DataPageLayout.scss';
 
 export default function DataPageLayout() {
    const appStatus = useSelector(selectStatus);
+   const menuIsOpen = useSelector(selectMenuStatus);
+
+   const dispatch = useDispatch();
+   const ref = useRef();
 
    return appStatus === loadingStatus.ERROR ? (
       <ErrorPage />
@@ -19,9 +29,24 @@ export default function DataPageLayout() {
             <div className="DataPageLayout__header">
                <Header />
             </div>
-            <div className="DataPageLayout__burger"></div>
+            <div
+               className={`DataPageLayout__burger${
+                  menuIsOpen ? ' menuOpened' : ''
+               }`}
+               onClick={() => dispatch(toggleMenuStatus())}
+            >
+               <Burger />
+            </div>
             <div className="DataPageLayout__main">
-               <div className="DataPageLayout__menu"></div>
+               <CSSTransition
+                  in={menuIsOpen}
+                  timeout={5000}
+                  nodeRef={ref}
+                  mountOnEnter
+                  unmountOnExit
+               >
+                  <div className="DataPageLayout__menu" ref={ref}></div>
+               </CSSTransition>
             </div>
             <div className="DataPageLayout__footer">
                <Footer />
