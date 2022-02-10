8b416a5608d9b04e5015b18ca6280c949b510c0e
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-10 17:39:02 +0700

    Separate search block title

--- src/components/SearchBlock/SearchBlock.js
+++ src/components/SearchBlock/SearchBlock.js
@@ -1,24 +1,29 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectSearchValues } from '../../redux/reducers/data/dataSelectors';
 import SearchField from '../SearchField/SearchField';
 import { AiOutlineSearch } from 'react-icons/ai';
 import './SearchBlock.scss';
+import { sectionURLs } from '../../redux/common';
 
-export default function SearchBlock() {
-   const serchValues = useSelector(selectSearchValues);
+export default function SearchBlock({ dataType }) {
+   const searchValues = useSelector(selectSearchValues);
    const [activeField, setActiveField] = useState(null);
 
    return (
       <div className="SearchBlock">
          <div className="SearchBlock__header">
-            <h3 className="SearchBlock__title">Search by:</h3>
+            <h3 className="SearchBlock__title">
+               {sectionURLs.filter((url) => url.dataType === dataType)[0]
+                  .caption + ':'}
+            </h3>
             <div className="SearchBlock__icon">
                <AiOutlineSearch />
             </div>
          </div>
          <div className="SearchBlock__fields">
-            {serchValues.map((searchField) => (
+            {searchValues.map((searchField) => (
                <SearchField
                   {...searchField}
                   key={searchField.fieldName}
@@ -30,3 +35,7 @@ export default function SearchBlock() {
       </div>
    );
 }
+
+SearchBlock.propTypes = {
+   dataType: PropTypes.string.isRequired,
+};
