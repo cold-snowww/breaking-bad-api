4def9dd288f7e1fb18bc42714ce03c4cc3fb17ad
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-10 19:34:29 +0700

    Clear filters when WelcomePage renders

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -3,20 +3,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadingStatus } from '../../redux/common';
 import { selectMenuStatus } from '../../redux/reducers/app/appSelectors';
 import { toggleMenuStatus } from '../../redux/reducers/app/appSlice';
-import { selectStatus } from '../../redux/reducers/data/dataSelectors';
-import { setStatus } from '../../redux/reducers/data/dataSlice';
+import {
+   selectFilter,
+   selectStatus,
+} from '../../redux/reducers/data/dataSelectors';
+import { setFilter, setStatus } from '../../redux/reducers/data/dataSlice';
 import WelcomePageLayout from '../WelcomePageLayout/WelcomePageLayout';
 
 export default function WelcomePage() {
    const dispatch = useDispatch();
    const appStatus = useSelector(selectStatus);
+   const filter = useSelector(selectFilter);
 
-   // Clear errors if any exist
+   // Clear errors and filters if any exist
    useEffect(() => {
       if (appStatus === loadingStatus.ERROR) {
          dispatch(setStatus(loadingStatus.IDLE));
       }
-   }, [appStatus, dispatch]);
+      if (filter[0] || filter[1]) {
+         dispatch(setFilter(null, null));
+      }
+   }, [appStatus, filter, dispatch]);
 
    // Close menu if opened
    const menuIsOpened = useSelector(selectMenuStatus);
