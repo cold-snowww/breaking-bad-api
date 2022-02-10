a0967b98439a9a8bcae63017195dfd561e545d24
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-10 17:41:58 +0700

    Insert child components

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,18 @@
+import { useSelector } from 'react-redux';
+import { selectDataType } from '../../redux/reducers/data/dataSelectors';
+import RandomQuoteBtn from '../RandomQuoteBtn/RandomQuoteBtn';
 import SearchBlock from '../SearchBlock/SearchBlock';
+import MenuSectionLinks from '../MenuSectionLinks/MenuSectionLinks';
 import './Menu.scss';
 
 export default function Menu() {
+   const dataType = useSelector(selectDataType);
+
    return (
       <div className="Menu">
-         <SearchBlock />
+         <SearchBlock dataType={dataType} />
+         <RandomQuoteBtn dataType={dataType} />
+         <MenuSectionLinks dataType={dataType} />
       </div>
    );
 }
