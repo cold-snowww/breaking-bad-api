656ab9765106c7ad19c0ba5c87bcc51a774b09e3
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-14 15:21:15 +0700

    Clearing filter after move to another section

--- src/components/MenuSectionLinks/MenuSectionLinks.js
+++ src/components/MenuSectionLinks/MenuSectionLinks.js
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { sectionURLs } from '../../redux/common';
+import { clearFilter } from '../../redux/reducers/data/dataSlice';
 import './MenuSectionLinks.scss';
 
 export default function MenuSectionLinks({ dataType }) {
+   const dispatch = useDispatch();
+
    return (
       <div className="MenuSectionLinks">
          {sectionURLs
             .filter((url) => url.dataType !== dataType)
             .map(({ dataType, url, caption }) => (
-               <Link to={url} key={dataType} className="MenuSectionLinks__link">
+               <Link
+                  to={url}
+                  key={dataType}
+                  className="MenuSectionLinks__link"
+                  onClick={() => dispatch(clearFilter())}
+               >
                   {caption}
                </Link>
             ))}
