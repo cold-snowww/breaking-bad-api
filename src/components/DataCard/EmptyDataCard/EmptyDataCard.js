5f33ac34773c56be9c533c6b966ef17c96ca2c39
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-14 14:38:57 +0700

    Restructure component

--- src/components/DataCard/EmptyDataCard/EmptyDataCard.js
+++ src/components/DataCard/EmptyDataCard/EmptyDataCard.js
@@ -1,11 +1,10 @@
 import DataCard from '../DataCard';
 import { MdOutlineTipsAndUpdates } from 'react-icons/md';
-import './EmptyDataCard.scss';
 
 export default function EmptyDataCard() {
    return (
       <DataCard title="Tip..." icon={<MdOutlineTipsAndUpdates />}>
-         <p className="EmptyDataCard__text">
+         <p className="DataCard__text">
             To display data, use filters in the menu. Here you can find out more
             about one of the best TV series ever!
          </p>
