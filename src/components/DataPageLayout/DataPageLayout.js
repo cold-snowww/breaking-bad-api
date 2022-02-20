24c50ca9e85d272faf6d16caec2531b5b3ed2414
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-20 14:56:10 +0700

    Add blocks for desktop

--- src/components/DataPageLayout/DataPageLayout.js
+++ src/components/DataPageLayout/DataPageLayout.js
@@ -8,11 +8,13 @@ import { toggleMenuStatus } from '../../redux/reducers/app/appSlice';
 import { selectStatus } from '../../redux/reducers/data/dataSelectors';
 import Burger from '../Burger/Burger';
 import CardHolder from '../CardHolder/CardHolder';
+import Contacts from '../Contacts/Contacts';
 import ErrorPage from '../ErrorPage/ErrorPage';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Loader from '../Loader/Loader';
 import Menu from '../Menu/Menu';
+import MenuSectionLinks from '../MenuSectionLinks/MenuSectionLinks';
 import './DataPageLayout.scss';
 
 export default function DataPageLayout() {
@@ -38,6 +40,12 @@ export default function DataPageLayout() {
                onClick={() => dispatch(toggleMenuStatus())}
             >
                <Burger />
+            </div>
+            <div className="DataPageLayout__sectionsDesktop">
+               <MenuSectionLinks className="MenuSectionLinks-desktop" />
+            </div>
+            <div className="DataPageLayout__contactsDesktop">
+               <Contacts fixed />
             </div>
             <div className="DataPageLayout__main">
                <CSSTransition in={menuIsOpen} timeout={400} nodeRef={ref}>
