42c3a0c81681e76da4d7ac619a979fddaf12a391
Roman Kolmakov <cold.snowww@yahoo.com>
2022-02-20 10:44:33 +0700

    Add ErrorBoundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         error: null,
+      };
+   }
+
+   static getDerivedStateFromError(error) {
+      return { error };
+   }
+
+   componentDidCatch(error) {
+      console.error(error);
+   }
+
+   render() {
+      return this.state.error ? this.props.fallback : this.props.children;
+   }
+}
